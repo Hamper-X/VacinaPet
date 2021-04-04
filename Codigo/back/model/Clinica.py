fd31bf5fe0e0086816656ee72fb66f8c2b5c9eff
Yuri Braga <yuricancelabraga@gmail.com>
2021-04-04 19:08:32 -0300

    Tabela clinica

---
+++ Codigo/back/model/Clinica.py
@@ -0,0 +1,16 @@
+from service.database import db
+
+class Clinica(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  nome = db.Column(db.String(256))
+  veterinario = db.Column(db.String(256))
+  sintomas = db.Column(db.String(512))
+  prescricao = db.Column(db.String(1024))
+  pet_id = db.Column(db.Integer, db.ForeignKey('pet.id'), nullable=False)
+
+  def __init__(self, nome, veterinario, sintomas, prescricao, pet_id):
+    self.nome = nome
+    self.veterinario = veterinario
+    self.sintomas = sintomas
+    self.prescricao = prescricao
+    self.pet_id = pet_id
