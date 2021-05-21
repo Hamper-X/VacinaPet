e6811268e23e24a6c6dae151e65f0214fdd61c92
LuanaLins <luanaplins@gmail.com>
2021-05-21 02:42:43 -0300

    Produto está funcional
    Tanto Menu, Cadastro e Login estão funcionando pelo FrontEnd

--- Codigo/front/produto.py
+++ Codigo/front/produto.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from PIL import Image, ImageTk
 from tkinter import ttk
 
-LARGEFONT =("Verdana", 35)
+LARGEFONT =("Italic", 32)
   
 class tkinterApp(tk.Tk):
      
@@ -24,18 +24,18 @@ class tkinterApp(tk.Tk):
   
         # iterating through a tuple consisting
         # of the different page layouts
-        for F in (StartPage, Page1, Page2):
+        for F in (Menu, Login, Cadastrar):
   
             frame = F(container, self)
   
             # initializing frame of that object from
-            # startpage, page1, page2 respectively with
+            # Menu, Login, Cadastrar respectively with
             # for loop
             self.frames[F] = frame
   
             frame.grid(row = 0, column = 0, sticky ="nsew")
   
-        self.show_frame(StartPage)
+        self.show_frame(Menu)
   
     # to display the current frame passed as
     # parameter
@@ -46,97 +46,195 @@ class tkinterApp(tk.Tk):
 
 
   
-# first window frame startpage
+# first window frame Menu
   
-class StartPage(tk.Frame):
+class Menu(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+
+        # Abrindo Imagem
         photo = Image.open("vacpet.png")
-        resize = photo.resize((1000,600), Image.ANTIALIAS)
+        # Arrumando Tamanho
+        resize = photo.resize((1030,600), Image.ANTIALIAS)
+        # Criando a imagem
         render = ImageTk.PhotoImage(resize)
+        # Inserindo a imagem no label
         label = ttk.Label(self, image=render)
-
+        # Setando imagem do label com imagem criada
         label.image = render
-
+        # Colocando label no frame
         label.pack()
+        # Centralizando
         label.place(relx=0,rely=0)
+
+
+        button1 = tk.Button(self, text="Login", font= LARGEFONT, relief="solid",background="white", command=lambda : controller.show_frame(Login))
+        button2 = tk.Button(self, text="Cadastrar", font= LARGEFONT, relief="solid",background="white",command =lambda : controller.show_frame(Cadastrar))
+        espaco1 = tk.Label(self,pady=100,background="white")
+        espaco2 = tk.Label(self,pady=15,background="white")
+
+        espaco1.pack()
+        button1.pack()
+        espaco2.pack()
+        button2.pack()
          
-        # label of frame Layout 2
-        label = ttk.Label(self, text ="Startpage", font = LARGEFONT)
-         
-        # putting the grid in its place by using
-        # grid
-        label.grid(row = 0, column = 4, padx = 10, pady = 10)
-  
-        button1 = ttk.Button(self, text ="Page 1",
-        command = lambda : controller.show_frame(Page1))
-     
-        # putting the button in its place by
-        # using grid
-        button1.grid(row = 1, column = 1, padx = 10, pady = 10)
-  
-        ## button to show frame 2 with text layout2
-        button2 = ttk.Button(self, text ="Page 2",
-        command = lambda : controller.show_frame(Page2))
-     
-        # putting the button in its place by
-        # using grid
-        button2.grid(row = 2, column = 1, padx = 10, pady = 10)
   
           
   
   
-# second window frame page1
-class Page1(tk.Frame):
+# second window frame Login
+class Login(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text ="Page 1", font = LARGEFONT)
-        label.grid(row = 0, column = 4, padx = 10, pady = 10)
-        # button to show frame 2 with text
-        # layout2
-        button1 = ttk.Button(self, text ="StartPage",
-                            command = lambda : controller.show_frame(StartPage))
-     
-        # putting the button in its place
-        # by using grid
-        button1.grid(row = 1, column = 1, padx = 10, pady = 10)
+
+        photo = Image.open("login.jpg")
+        # Arrumando Tamanho
+        resize = photo.resize((1030,600), Image.ANTIALIAS)
+        # Criando a imagem
+        render = ImageTk.PhotoImage(resize)
+        # Inserindo a imagem no label
+        label = ttk.Label(self, image=render)
+        # Setando imagem do label com imagem criada
+        label.image = render
+        # Colocando label no frame
+        label.pack()
+        # Centralizando
+        label.place(relx=0,rely=0)
+
+        spaco= tk.Label(self,pady=65)
+        spaco.pack()
+        spaco.place()
+
+        btn_voltar = tk.Button(self,width=10,text="Voltar",command=lambda:controller.show_frame(Menu),font=("Italic", 14), relief="solid",background="white")
+        btn_voltar.place(x=25,y=25)
+
+        lbl_email = tk.Label(self, width = 20, text="email:", font=("Italic", 16))
+        lbl_senha = tk.Label(self, width = 20, text="senha:", font=("Italic", 16))
+
+
+        ent_email = tk.Entry(self, width = 40, relief="solid",bg='white')
+        ent_senha = tk.Entry(self, width = 40, relief="solid",bg='white')
+        
+        space = tk.Canvas(self, width = 40, height=10)
+        btn_enviar = tk.Button(self, width = 10, text="Login",command = lambda:self.ent_verification(ent_email,ent_senha), font=("Italic", 14), relief="solid",bg='white')
+
+
+        lbl_email.pack()
+        ent_email.pack()
+        lbl_senha.pack()
+        ent_senha.pack()
+
+        space.pack()
+        btn_enviar.pack()
+
+    def ent_verification(self,email,senha):
+        data = []
+        data.append(str(email.get()))
+        data.append(str(senha.get()))
+        resp = False
+
+        cad=tk.Tk()
+
+        if data[0] == "" or data[0]==" ":
+            lbl_Alert = tk.Label(cad, width = 50, text="Ocorreu um imprevisto, seu email não foi inserido.", font=("Italic", 13))
+            btn_enviar = tk.Button(cad, width = 6, text="OK",command=cad.destroy, font=("Italic", 10), relief="solid",bg='white')
+            resp = True
+            lbl_Alert.pack()
+            btn_enviar.pack()
+
+        else: 
+            if data[1] == "" or data[1]==" ":
+                lbl_Alert = tk.Label(cad, width = 50, text="Ocorreu um imprevisto, sua senha não foi inserida.", font=("Italic", 13))
+                btn_enviar = tk.Button(cad, width = 6, text="OK",command=cad.destroy, font=("Italic", 10), relief="solid",bg='white')
+                resp = True
+                lbl_Alert.pack()
+                btn_enviar.pack()
+            else:
+                lbl_Alert = tk.Label(cad, width = 50, text="Tudo Certo, Confirme para Prosseguir", font=("Italic", 13))
+                btn_enviar = tk.Button(cad, width = 6, text="OK",command=cad.destroy, font=("Italic", 10), relief="solid",bg='white')
+                resp = True
+                lbl_Alert.pack()
+                btn_enviar.pack()
+
+
   
-        # button to show frame 2 with text
-        # layout2
-        button2 = ttk.Button(self, text ="Page 2",
-                            command = lambda : controller.show_frame(Page2))
-     
-        # putting the button in its place by
-        # using grid
-        button2.grid(row = 2, column = 1, padx = 10, pady = 10)
   
   
   
-  
-# third window frame page2
-class Page2(tk.Frame):
+# third window frame Cadastrar
+class Cadastrar(tk.Frame):
     def __init__(self, parent, controller):
+
         tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text ="Page 2", font = LARGEFONT)
-        label.grid(row = 0, column = 4, padx = 10, pady = 10)
-  
-        # button to show frame 2 with text
-        # layout2
-        button1 = ttk.Button(self, text ="Page 1",
-                            command = lambda : controller.show_frame(Page1))
-     
-        # putting the button in its place by
-        # using grid
-        button1.grid(row = 1, column = 1, padx = 10, pady = 10)
-  
-        # button to show frame 3 with text
-        # layout3
-        button2 = ttk.Button(self, text ="Startpage",
-                            command = lambda : controller.show_frame(StartPage))
-     
-        # putting the button in its place by
-        # using grid
-        button2.grid(row = 2, column = 1, padx = 10, pady = 10)
+        photo = Image.open("login.jpg")
+        # Arrumando Tamanho
+        resize = photo.resize((1030,600), Image.ANTIALIAS)
+        # Criando a imagem
+        render = ImageTk.PhotoImage(resize)
+        # Inserindo a imagem no label
+        label = ttk.Label(self, image=render)
+        # Setando imagem do label com imagem criada
+        label.image = render
+        # Colocando label no frame
+        label.pack()
+        # Centralizando
+        label.place(relx=0,rely=0)
+
+        btn_voltar = tk.Button(self,width=10,text="Voltar",command=lambda:controller.show_frame(Menu),font=("Italic", 14), relief="solid",background="white")
+        btn_voltar.place(x=25,y=25)
+
+        lbl_nome = tk.Label(self, width = 20, text="Nome:", font=("Italic", 16))
+        lbl_email = tk.Label(self, width = 20, text="email:", font=("Italic", 16))
+        lbl_senha = tk.Label(self, width = 20, text="senha:", font=("Italic", 16))
+
+        ent_nome = tk.Entry(self, width = 40, relief="solid",bg='white')
+        ent_email = tk.Entry(self, width = 40, relief="solid",bg='white')
+        ent_senha = tk.Entry(self, width = 40, relief="solid",bg='white')
+        
+        btn_enviar = tk.Button(self, width = 10, text="Cadastre-se",command = lambda:self.ent_verification(self,ent_nome,ent_email,ent_senha), font=("Italic", 14), relief="solid",bg='white')
+
+        lbl_nome.place(x=500, y=150,anchor="center")
+        ent_nome.place(x=500, y=175,anchor="center")
+        lbl_email.place(x=500, y=200,anchor="center")
+        ent_email.place(x=500, y=225,anchor="center")
+        lbl_senha.place(x=500, y=250,anchor="center")
+        ent_senha.place(x=500, y=275,anchor="center")
+        
+        btn_enviar.place(x=500, y=450,anchor="center")
+    
+    def ent_verification(self,cadastro,nome,email,senha):
+        data = []
+        data.append(str(nome.get()))
+        data.append(str(email.get()))
+        data.append(str(senha.get()))
+        resp = False
+
+
+        cad = tk.Tk()
+        cad.wm_attributes("-topmost", True)
+        cad.title("Cadastro")
+        cad.geometry("395x120")
+
+
+
+        if data[0] == "" or data[0]==" ":
+            lbl_Alert = tk.Label(cad, width = 30, text="ERRO! A aba de nome deve ser preenchida.", font=("Italic", 12))
+            resp = True
+        if data[1] == "" or data[1]==" ":
+            lbl_Alert = tk.Label(cad, width = 30, text="ERRO! A aba de email deve ser preenchida.", font=("Italic", 12))
+            resp = True
+        if data[2] == "" or data[2]==" ":
+            lbl_Alert = tk.Label(cad, width = 30, text="ERRO! A aba de senha deve ser preenchida.", font=("Italic", 12))
+            resp = True
+        if resp==False: 
+            lbl_Alert = tk.Label(cad, width = 30, text="Cadastro realizado!", font=("Italic", 14))    
+
+
+        space = tk.Canvas(cad, width = 40, height=10)
+        btn_enviar = tk.Button(cad, width = 6, text="OK",command=cad.destroy, font=("Italic", 14), relief="solid",bg='white')
+        lbl_Alert.pack()
+        space.pack()
+        btn_enviar.pack()
   
   
 # Driver Code
