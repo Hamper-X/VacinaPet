4995d61959a82711981d023b2ca508d687794e18
LuanaLins <luanaplins@gmail.com>
2021-05-29 16:27:53 -0300

    Quase
    Visualemnte algumas coisas

--- Codigo/front/produto.py
+++ Codigo/front/produto.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from PIL import Image, ImageTk
-from tkinter import Tk, ttk
+from tkinter import PhotoImage, Tk, ttk
 
 LARGEFONT =("Italic", 32)
   
@@ -20,11 +20,11 @@ class tkinterApp(tk.Tk):
         container.grid_columnconfigure(0, weight = 1)
   
         # initializing frames to an empty array
-        self.frames = {} 
+        self.frames = {}
   
         # iterating through a tuple consisting
         # of the different page layouts
-        for F in (Menu, Login, Cadastrar, BuscasPet):
+        for F in (Menu, Login, Cadastrar, BuscasPet, CadastroPet, BuscarPet, BuscarClinica, Relatorio, MostrarPets):
   
             frame = F(container, self)
   
@@ -224,24 +224,173 @@ class Cadastrar(tk.Frame):
         btn_enviar.pack()
 
 
+
 class BuscasPet(tk.Frame):
     def __init__(self,parent,controller):
         tk.Frame.__init__(self, parent)
-        espaco = tk.Label(self,pady=15,bg='white')
-        btn_CadPet = tk.Button(self, width = 15, text="Cadastro Pet", font=("Italic", 32), relief="solid",bg='white')
-        btn_MostrarPet = tk.Button(self, width=15, text="Mostrar Pet",command = self.register, font=("Italic", 32), relief="solid",bg='white')
-        btn_MostrarClinica = tk.Button(self, width=15, text="Mostrar Clinicas",command = self.register, font=("Italic", 32), relief="solid",bg='white')
 
-        espaco.pack()
-        btn_CadPet.pack()
-        espaco.pack()
-        btn_MostrarPet.pack()
-        espaco.pack()
-        btn_MostrarClinica.pack()
-        espaco.pack()
+        #Abrindo Imagem
+        photo = Image.open("backgroundMenu.jpg")
+        # Arrumando Tamanho
+        resize = photo.resize((1030,600), Image.ANTIALIAS)
+        # Criando a imagem
+        render = ImageTk.PhotoImage(resize)
+        # Inserindo a imagem no label
+        label = ttk.Label(self, image=render)
+        # Setando imagem do label com imagem criada
+        label.image = render
+        # Colocando label no frame
+        label.pack()
+        # Centralizando
+        label.place(relx=0,rely=0)
 
+        self.icone1 = PhotoImage(file="iconeAdd.png")
+        self.pi1 = self.icone1.subsample(1, 1)
+        self.icone2 = PhotoImage(file="iconePesquisaPet.png")
+        self.pi2 = self.icone2.subsample(1, 1)
+        self.icone3 = PhotoImage(file="iconeClinica.png")
+        self.pi3 = self.icone3.subsample(1,1)
+        self.icone4 = PhotoImage(file="iconeRelatorio.png")
+        self.pi4 = self.icone4.subsample(1,1)
+        self.icone5 = PhotoImage(file="iconeLista.png")
+        self.pi5 = self.icone5.subsample(1,1)
+
+
+        btn_CadPet = tk.Button(self,image=self.pi1,width=215, text="Cadastro Animal", command = lambda:controller.show_frame(CadastroPet), font=("Italic", 20), relief="solid",bg='white',compound="top")
+        btn_BuscarPet = tk.Button(self, image=self.pi2,width=215, text="Buscar Pet",command = lambda:controller.show_frame(BuscarPet), font=("Italic", 20), relief="solid",bg='white',compound="top")
+        btn_BuscarClinica = tk.Button(self, image=self.pi3,width=215, text="Buscar Clinica",command = lambda:controller.show_frame(BuscarClinica), font=("Italic", 20), relief="solid",bg='white',compound="top")
+        btn_Relatorio = tk.Button(self, image=self.pi4,width=215, text="Relatório",command = lambda:controller.show_frame(Relatorio), font=("Italic", 20), relief="solid",bg='white',compound="top")
+        btn_MostrarPets = tk.Button(self, image=self.pi5,width=215, text="Lista Animais",command = lambda:controller.show_frame(MostrarPets), font=("Italic", 20), relief="solid",bg='white',compound="top")
+
+        btn_CadPet.place(x=275,y=100)
+        btn_BuscarPet.place(x=525,y=100)
+        btn_BuscarClinica.place(x=275,y=250)
+        btn_MostrarPets.place(x=525, y=250)
+        btn_Relatorio.place(x=400, y=400)
+
+
+class CadastroPet(tk.Frame):
+    def __init__(self,parent,controller):
+        tk.Frame.__init__(self, parent)
+        
+        btn_voltar = tk.Button(self,width=10,text="Voltar",command=lambda:controller.show_frame(BuscasPet),font=("Italic", 14), relief="solid",background="white")
+        btn_voltar.place(x=25,y=25)
+
+        lbl_nome = tk.Label(self, width = 30, text="Nome:", font=("Italic", 16))
+        lbl_especie = tk.Label(self, width = 30, text="Espécie:", font=("Italic", 16))
+        lbl_peso = tk.Label(self, width = 30, text="Peso:", font=("Italic", 16))
+        lbl_sexo = tk.Label(self, width = 30, text="Sexo:", font=("Italic", 16))
+        lbl_porte = tk.Label(self, width = 30, text="Porte:", font=("Italic", 16))
+        lbl_nascimento = tk.Label(self, width = 30, text="Nascimento:", font=("Italic", 16))
+        lbl_raca = tk.Label(self, width = 30, text="Raça:", font=("Italic", 16))
+
+        ent_nome = tk.Entry(self, width = 40, relief="solid",bg='white')
+        ent_especie = tk.Entry(self, width = 40, relief="solid",bg='white')
+        ent_peso = tk.Entry(self, width = 40, relief="solid",bg='white')
+        ent_sexo = tk.Entry(self, width = 40, relief="solid",bg='white')
+        ent_porte = tk.Entry(self, width = 40, relief="solid",bg='white')
+        ent_nascimento = tk.Entry(self, width = 40, relief="solid",bg='white')
+        ent_raca = tk.Entry(self, width = 40, relief="solid",bg='white')
+        
+        cad = tk.Button(self, width=15, text="Cadastrar", command = lambda:self.cadastra(ent_nome, ent_especie, ent_peso, ent_sexo,ent_porte,ent_nascimento,ent_raca), font=("Italic", 16))
+
+        lbl_nome.pack()
+        ent_nome.pack()
+        lbl_especie.pack()
+        ent_especie.pack()
+        lbl_peso.pack()
+        ent_peso.pack()
+        lbl_sexo.pack()
+        ent_sexo.pack()
+        lbl_porte.pack()
+        ent_porte.pack()
+        lbl_nascimento.pack()
+        ent_nascimento.pack()
+        lbl_raca.pack()
+        ent_raca.pack()
+        cad.pack()
+
+    def cadastra(self, nome, especie, peso, sexo, porte, nascimento, raca):
+        pass
+
+class BuscarPet(tk.Frame):
+    def __init__(self,parent,controller):
+        tk.Frame.__init__(self, parent)
+        
+        btn_voltar = tk.Button(self,width=10,text="Voltar",command=lambda:controller.show_frame(BuscasPet),font=("Italic", 14), relief="solid",background="white")
+        btn_voltar.place(x=25,y=25)
+
+        lbl_busca = tk.Label(self, width = 30, text="Nome do Animal:", font=("Italic", 16))
+        ent_busca = tk.Entry(self, width = 40, relief="solid",bg='white')
+
+        lbl_nome = tk.Label(self, width = 30, text="Nome:", font=("Italic", 16))
+        lbl_especie = tk.Label(self, width = 30, text="Espécie:", font=("Italic", 16))
+        lbl_peso = tk.Label(self, width = 30, text="Peso:", font=("Italic", 16))
+        lbl_sexo = tk.Label(self, width = 30, text="Sexo:", font=("Italic", 16))
+        lbl_porte = tk.Label(self, width = 30, text="Porte:", font=("Italic", 16))
+        lbl_nascimento = tk.Label(self, width = 30, text="Nascimento:", font=("Italic", 16))
+        lbl_raca = tk.Label(self, width = 30, text="Raça:", font=("Italic", 16))
+
+        ent_nome = tk.Entry(self, width = 40, relief="solid",bg='white')
+        ent_especie = tk.Entry(self, width = 40, relief="solid",bg='white')
+        ent_peso = tk.Entry(self, width = 40, relief="solid",bg='white')
+        ent_sexo = tk.Entry(self, width = 40, relief="solid",bg='white')
+        ent_porte = tk.Entry(self, width = 40, relief="solid",bg='white')
+        ent_nascimento = tk.Entry(self, width = 40, relief="solid",bg='white')
+        ent_raca = tk.Entry(self, width = 40, relief="solid",bg='white')
+        
+        pesquisa = tk.Button(self, width=15, text="Pesquisar", command = lambda:self.pesquisa(ent_busca), font=("Italic", 16))
+        pesquisa.place(x=25,y=975)
+
+        lbl_busca.pack()
+        ent_busca.pack()
+        lbl_nome.pack()
+        ent_nome.pack()
+        lbl_especie.pack()
+        ent_especie.pack()
+        lbl_peso.pack()
+        ent_peso.pack()
+        lbl_sexo.pack()
+        ent_sexo.pack()
+        lbl_porte.pack()
+        ent_porte.pack()
+        lbl_nascimento.pack()
+        ent_nascimento.pack()
+        lbl_raca.pack()
+        ent_raca.pack()
+
+
+    def pesquisa(self, nome):
+        pass
   
-  
+
+class BuscarClinica(tk.Frame):
+    def __init__(self,parent,controller):
+        tk.Frame.__init__(self, parent)  
+
+        btn_voltar = tk.Button(self,width=10,text="Voltar",command=lambda:controller.show_frame(BuscasPet),font=("Italic", 14), relief="solid",background="white")
+        btn_voltar.place(x=25,y=25)
+        pass
+
+class Relatorio(tk.Frame):
+    def __init__(self,parent,controller):
+        tk.Frame.__init__(self, parent)
+
+        btn_voltar = tk.Button(self,width=10,text="Voltar",command=lambda:controller.show_frame(BuscasPet),font=("Italic", 14), relief="solid",background="white")
+        btn_voltar.place(x=25,y=25)
+        pass
+
+class MostrarPets(tk.Frame):
+    def __init__(self,parent,controller):
+        tk.Frame.__init__(self, parent)
+
+        btn_voltar = tk.Button(self,width=10,text="Voltar",command=lambda:controller.show_frame(BuscasPet),font=("Italic", 14), relief="solid",background="white")
+        btn_voltar.place(x=25,y=25)
+        pass
+
+
+
+
 # Driver Code
 app = tkinterApp()
 app.mainloop()
